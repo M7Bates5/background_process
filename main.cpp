90ebba0c11c11b8d3e0bd107cfe9ed9c9ed84323
M7bates5 <m.tango.bravo@gmail.com>
2017-02-26 17:52:13 -0500

    Changes to be committed: 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@
 
 #include <iostream>
 
-int main(){
+int main(int argc, char *argv[])
+{
+    std::cout << argv[0] << std::endl;
+    for(int i=1; i<(sizeof(argv)/sizeof(char)); i++)
+    {
+      std::cout<<argv[i]<<"\n";
+    }
+    #ifdef __linux__
+      std::cout<<"linux baby!!";
+    #endif // __linux__
+    #ifdef _WIN32
+      std::cout<<"This is Windows!!\n";
+    #endif // _WIN32
+    #ifdef macintosh
+      std::cout<<"this is not windows!!\n";
+    #endif // macintosh
+    #ifdef sun
+    std::cout<<"still not windows"\n;
+    #endif // sun
 
+    return 0;
 }
